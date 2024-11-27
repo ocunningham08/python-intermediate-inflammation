708823205f39373a8b7b34a1aef3428cf3ccdecb
Oliver Cunningham <ocunningham08@qub.ac.uk>
2024-11-27 16:37:14 +0000

    decoupled analyse_data function

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -8,17 +8,21 @@ import numpy as np
 from inflammation import models, views
 
 
-def analyse_data(data_dir):
-    """Calculates the standard deviation by day between datasets.
-
-    Gets all the inflammation data from CSV files within a directory,
-    works out the mean inflammation value for each day across all datasets,
-    then plots the graphs of standard deviation of these means."""
+def load_inflammation_dat(data_dir):
+    """Reads in the inflammation CSV files"""
     data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
     if len(data_file_paths) == 0:
         raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
     data = map(models.load_csv, data_file_paths)
+    return list(data)
 
+def analyse_data(data_dir):
+    """Calculates the standard deviation by day between datasets.
+
+    Works out the mean inflammation value for each day across all datasets,
+    then plots the graphs of standard deviation of these means."""
+    
+    data = load_inflammation_data(data_dir)
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
